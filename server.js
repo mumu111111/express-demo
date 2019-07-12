bde1cacd4903a11ca0fd8d478132d13c76d5d258
mumu111111 <2646709035@qq.com>
2019-07-12 21:32:14 +0800

    'update'

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const app = express()
 const path = require('path')
 
 app.get('/', (req,res)=>{
-    let p = path.join('__dirname', ',./index.html')
+    let p = path.join(__dirname, './index.html')
     res.sendFile(p)
 })
 
 app.get('/xx', (req,res)=>{
-    let p = path.join('__dirname', ',./index.html')
     res.sendFile('hello')
 })
+
+app.use('/static', express.static(path.join(__dirname, 'public')))//路由/static 映射 public文件夹
 app.listen(9999, ()=>{console.log('Exaple app listening on port 9999')})
 
